f1b874acffb8fb7a13730cd55f8c3bffa9ee55a6
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2023-04-07 21:26:03 +0530

    Date changed

--- src/components/NewsApp.js
+++ src/components/NewsApp.js
@@ -5,13 +5,13 @@ function NewsApp() {
 
     const apiKey = 'e0f8f3eb51424a6cb1bba79036ddbc2a';
 
-
+    //https://newsapi.org/v2/everything?q=premierleague&from=2023-03-05&sortBy=publishedAt&apiKey=e0f8f3eb51424a6cb1bba79036ddbc2a
     const [newsList, setnewsList] = useState([]);
     const [query, setquery] = useState('premierleague');
 
     const queryRef = useRef(null)
 
-    const apiurl = `https://newsapi.org/v2/everything?q=${query}&from=2023-03-05&sortBy=publishedAt&apiKey=${apiKey}`
+    const apiurl = `https://newsapi.org/v2/everything?q=${query}&from=2023-04-05&sortBy=publishedAt&apiKey=${apiKey}`
     useEffect(() => {
         fetchData();
     }, [query])
@@ -39,10 +39,11 @@ function NewsApp() {
             <h2 className='header' >NewsApp</h2>
             <p>Type below to search</p>
             <form onSubmit={handleSubmit}>
-                <input className='query-input' type='text' ref={queryRef}/>
+                <input className='query-input' type='text' ref={queryRef} />
                 <input className='btn' onClick={handleSubmit} type='submit' value='Submit' />
             </form>
             <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2,48%)', justifyContent: 'space-between', rowGap: '20px' }}>
+                
                 {newsList.map(news => {
                     // return <p>{news.title}</p>
                     return <News news={news} />;
